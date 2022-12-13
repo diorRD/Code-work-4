5b7fc923c6bd24f475feb95f72692fb626a25bc1
diorRD <diyorbekraximov899@gmail.com>
2022-12-13 19:35:09 +0500

    Array

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//? Data structure ==> Array
+
+// const array = ["Botir", "Qodir", "Sobir"]
+
+// console.log(array[1]);
+// const user = {
+//    name: "Pistonchi",
+//    friends: {
+//        friend1: "Botir",
+//     friend2: "Qodir",
+//        friend3: "Eshmat",
+//    },
+// };
+// user.name;
+// user["name"];
+  
+// const user = {
+//    name:"Pistonchi",
+//    friends: ["Botir", "Sobir", "Qodir"]
+// };
+
+// const array = [
+//    { name: "Pistonchi" },
+//    ["Hello", "Salom", "Guten abend", "Gute nacht"],
+//    2912912,
+//    "Sobir",
+//    "Qodir",
+//    true,
+//    NaN,
+//    null,
+//    undefined,
+//    function () {
+//        console.log("Hello world");
+//    },
+// ];
+// console.log(array.length);
